ed87ca00ec3c8ca0dfe5f2845e6571134884e5dd
kunaljunkie <109500415+kunaljunkie@users.noreply.github.com>
2024-08-13 14:42:46 +0530

    change

--- src/components/services/service.js
+++ src/components/services/service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:5001/api/v1/"
+const baseURL = "https://shark-striker-backend.onrender.com/"
 const api = axios.create({
   baseURL: baseURL,
   headers: {
